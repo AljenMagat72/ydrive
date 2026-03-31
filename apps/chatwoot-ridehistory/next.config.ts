a9792302ea31bece1772646d7b432feb784ae815
Aljenm72 <aljen@ydriveapp.com>
2026-03-31 21:53:30 +0700

    Feature/chatwoot build fix (#66)

--- apps/chatwoot-ridehistory/next.config.ts
+++ apps/chatwoot-ridehistory/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
   allowedDevOrigins: ['laverne-recriminative-nonheroically.ngrok-free.dev'],
 };
 
