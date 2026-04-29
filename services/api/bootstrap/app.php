92b31e60366dcd8b9ca5a6becdd2c374868ef85c
Steven Adams <steven.adams.r@gmail.com>
2026-04-29 17:59:09 -0400

    [Feature] Sentry (#93)

--- services/api/bootstrap/app.php
+++ services/api/bootstrap/app.php
@@ -12,6 +12,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 use Laravel\Sanctum\Http\Middleware\CheckAbilities;
 use Laravel\Sanctum\Http\Middleware\CheckForAnyAbility;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
   ->withRouting(
@@ -38,6 +39,7 @@ return Application::configure(basePath: dirname(__DIR__))
   })
 
   ->withExceptions(function (Exceptions $exceptions) {
+    Integration::handles($exceptions);
     $exceptions->shouldRenderJsonWhen(function ($request, $e) {
       if ($request->is('api/*')) {
         return true;
