f7bdc4c12f61039400b9f515ed12f5634fbe001b
Steven Adams <steven.adams.r@gmail.com>
2026-05-28 10:36:39 -0400

    [Hotfix] adjusted return type for drivers (#112)

--- services/api/app/Http/Resources/Driver/DriverCityResource.php
+++ services/api/app/Http/Resources/Driver/DriverCityResource.php
@@ -2,24 +2,32 @@
 
 namespace App\Http\Resources\Driver;
 
+use App\Enums\Role;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class DriverCityResource extends JsonResource
 {
-  /**
-   * Transform the resource into an array.
-   *
-   * @return array<string, mixed>
-   */
-  public function toArray(Request $request): array
-  {
-    return [
-      'id' => $this->id,
-      'firstName' => $this->first_name,
-      'lastName' => $this->last_name,
-      'phoneNumber' => $this->phone_number,
-      'cityId' => $this->city_id,
-    ];
-  }
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $isAdmin = $request->user()->role == Role::ADMIN;
+
+        return [
+            'id' => $this->id,
+            'cityId' => $this->city_id,
+            ...($isAdmin ? [
+                'firstName' => $this->first_name,
+                'lastName' => $this->last_name,
+                'phoneNumber' => $this->phone_number,
+            ] : [
+                'firstName' => 'Driver',
+                'lastName' => ' ',
+            ]),
+        ];
+    }
 }
