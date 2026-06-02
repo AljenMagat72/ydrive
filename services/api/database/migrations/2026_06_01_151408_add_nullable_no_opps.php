8d6dff6c3eec6bc74e682b7c44252d06f43dee38
Steven Adams <steven.adams.r@gmail.com>
2026-06-02 14:22:31 -0400

    [fix] duplicate migration

--- services/api/database/migrations/2026_06_01_151408_add_nullable_no_opps.php
+++ services/api/database/migrations/2026_06_01_151408_add_nullable_no_opps.php
@@ -12,8 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table("vendor_lists", function (Blueprint $table) {
-            $table->string('no_opps_name')->nullable()->change();
-            $table->unsignedBigInteger('no_opps_id')->nullable()->change();
+
         });
     }
 
